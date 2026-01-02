dc01e3d4269967db72ce89e7d31f42349b2abc49
Lukas Dullinger <itislu.git@gmail.com>
2026-01-02 19:10:53 +0100

    refactor: Separate db and wordpress user
    Also remove unnecessary "admin" db user.

--- srcs/requirements/wordpress/tools/docker-entrypoint.sh
+++ srcs/requirements/wordpress/tools/docker-entrypoint.sh
@@ -17,13 +17,13 @@ if [ ! -f "/var/www/html/wp-config.php" ]; then
 		--allow-root \
 		--url="${DOMAIN_NAME}" \
 		--title="${WP_TITLE}" \
-		--admin_user="${MYSQL_WP_ADMIN}" \
-		--admin_password="${MYSQL_WP_ADMIN_PASSWORD}" \
+		--admin_user="${WP_ADMIN}" \
+		--admin_password="${WP_ADMIN_PASSWORD}" \
 		--admin_email="${WP_ADMIN_EMAIL}"
 
-	wp user create "${MYSQL_USER}" "${WP_USER_EMAIL}" \
+	wp user create "${WP_USER}" "${WP_USER_EMAIL}" \
 		--allow-root \
-		--user_pass="${MYSQL_PASSWORD}"
+		--user_pass="${WP_USER_PASSWORD}"
 fi
 
 exec php-fpm83 -F
