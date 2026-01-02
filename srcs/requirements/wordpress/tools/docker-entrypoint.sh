3b9a67f9c503dc8c882b6508d4902ea888419a11
Lukas Dullinger <itislu.git@gmail.com>
2026-01-02 18:09:30 +0100

    refactor: Quote all env variables in shell scripts

--- srcs/requirements/wordpress/tools/docker-entrypoint.sh
+++ srcs/requirements/wordpress/tools/docker-entrypoint.sh
@@ -11,22 +11,22 @@ if [ ! -f "/var/www/html/wp-config.php" ]; then
 
 	wp config create \
 		--allow-root \
-		--dbname=${MYSQL_DATABASE} \
-		--dbuser=${MYSQL_USER} \
-		--dbpass=${MYSQL_PASSWORD} \
+		--dbname="${MYSQL_DATABASE}" \
+		--dbuser="${MYSQL_USER}" \
+		--dbpass="${MYSQL_PASSWORD}" \
 		--dbhost=mariadb
 
 	wp core install \
 		--allow-root \
-		--url=${DOMAIN_NAME} \
+		--url="${DOMAIN_NAME}" \
 		--title="${WP_TITLE}" \
-		--admin_user=${MYSQL_WP_ADMIN} \
-		--admin_password=${MYSQL_WP_ADMIN_PASSWORD} \
-		--admin_email=${WP_ADMIN_EMAIL}
+		--admin_user="${MYSQL_WP_ADMIN}" \
+		--admin_password="${MYSQL_WP_ADMIN_PASSWORD}" \
+		--admin_email="${WP_ADMIN_EMAIL}"
 
-	wp user create ${MYSQL_USER} ${WP_USER_EMAIL} \
+	wp user create "${MYSQL_USER}" "${WP_USER_EMAIL}" \
 		--allow-root \
-		--user_pass=${MYSQL_PASSWORD}
+		--user_pass="${MYSQL_PASSWORD}"
 fi
 
 exec php-fpm83 -F
